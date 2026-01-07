039a6957a43b59197218e5bcf3ecc67d4262ed11
Andrey Bilenko <olimp_72@ukr.net>
2026-01-07 14:02:40 +0200

    Fix_Bags_Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -17,16 +17,9 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(32, 6)
-    assert result == sorted(result)
-    assert result == [5, 5, 5, 5, 6, 6]
-
-    result_diff = split_integer(17, 4)
-    assert result_diff == [4, 4, 4, 5]
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    # If we split 2 into 5 parts, we need three 0s and two 1s
     assert split_integer(2, 5) == [0, 0, 0, 1, 1]
-    # If we split 0 into 3 parts, we get three 0s
-    assert split_integer(0, 3) == [0, 0, 0]
